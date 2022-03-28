b1fa77c01238622dc372476ec04e9f9d34eab858
Tobias Carvalho <tobias.jung.carvalho@gmail.com>
2022-03-28 16:03:36 -0300

    completed project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoList } from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Roboto:wght@400;500;700&display=swap');
+      </style>
+      <div className="todo-app">
+        <TodoList />
+      </div>
+    </>
   );
 }
 
